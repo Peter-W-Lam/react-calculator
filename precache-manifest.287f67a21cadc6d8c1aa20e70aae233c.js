897bd1955b51c15c0ec71d8240d03623e50c0b78
Peter Lam <Peter.Lam@tufts.edu>
2019-10-10 15:19:05 -0400

    Updates

---
+++ precache-manifest.287f67a21cadc6d8c1aa20e70aae233c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "206f10992f61ef380cd02e895ddd23b0",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "2da50955fb7c04b3c09c",
+    "url": "/react-calculator/static/css/main.b966a809.chunk.css"
+  },
+  {
+    "revision": "5b537948f24afc95a481",
+    "url": "/react-calculator/static/js/2.5dbd8ed4.chunk.js"
+  },
+  {
+    "revision": "2da50955fb7c04b3c09c",
+    "url": "/react-calculator/static/js/main.c0b6a909.chunk.js"
+  },
+  {
+    "revision": "e546b3e323215319a407",
+    "url": "/react-calculator/static/js/runtime~main.00b8a6fe.js"
+  },
+  {
+    "revision": "6ae6785e6b3172659a942dd58dfe8f34",
+    "url": "/react-calculator/static/media/iphone.6ae6785e.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-calculator/static/media/logo.5d5d9eef.svg"
+  }
+]);
